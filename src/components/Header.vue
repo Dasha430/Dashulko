972fad12af949cb2e0f003c5d9fc843d30b57246
4 <you@example.com>
2025-09-10 11:31:51 +0300

    dfsvdfv

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template lang="">
+   <div class="head">
+        <div class="logo">
+
+        </div>
+        <div class="logo">
+
+        </div>
+        <div class="logo">
+
+        </div>
+   </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
